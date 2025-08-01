c09aa5cfdfb217ce8a206bbd0adf046ac13e7efe
anushagorige <gorigeanusha5703@gmail.com>
2025-08-01 20:33:58 +0530

    Fix: Clerk middleware for production

--- middleware.ts
+++ middleware.ts
@@ -3,4 +3,6 @@ import { authMiddleware } from "@clerk/nextjs/server";
 export default authMiddleware({
   publicRoutes: ["/", "/sign-in", "/sign-up"],
 });
-
+export const config = {
+matcher: ["/((?!_next|.\..).*)"], // protect everything except static files and public assets
+};
